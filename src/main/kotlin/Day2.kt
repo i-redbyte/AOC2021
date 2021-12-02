87fe1c67de26141eb1e75aad117fb05f8bc410e6
Ilya Sokolov <developer.sokolov@gmail.com>
2021-12-02 23:47:15 +0300

    @feat: remove enum

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,9 +1,3 @@
-enum class Direction {
-    up,
-    down,
-    forward
-}
-
 data class Position(
     var horizontal: Int = 0,
     var depth: Int = 0,
@@ -13,22 +7,21 @@ data class Position(
 }
 
 fun main() {
-
     val data: List<Pair<String, Int>> = readInputFile("day2")
         .map { it.split(" ") }
         .map { it.first() to it.last().toInt() }
     val position = Position()
     for (d in data) {
-        when (Direction.valueOf(d.first)) {
-            Direction.up -> {
+        when (d.first) {
+            "up" -> {
 //                position.depth -= d.second //first part
                 position.aim -= d.second
             }
-            Direction.down -> {
+            "down" -> {
 //                position.depth += d.second //first part
                 position.aim += d.second
             }
-            Direction.forward -> {
+            "forward" -> {
                 position.horizontal += d.second
                 position.depth += d.second * position.aim
             }

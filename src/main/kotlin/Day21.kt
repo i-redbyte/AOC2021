6d4f19df3b7cb1906052b80a4058fa394a12740f
Ilya Sokolov <developer.sokolov@gmail.com>
2022-01-12 03:10:26 +0300

    solution day 21 (part 2)

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,10 +1,38 @@
+typealias Cube = Array<Array<Array<Array<WorldCopy?>>>>
+
+data class WorldCopy(var w1: Long, var w2: Long) {
+    companion object {
+        var cube: Cube? = null
+
+        fun find(p1: Int, p2: Int, s1: Int, s2: Int): WorldCopy {
+            cube?.get(p1)?.get(p2)?.get(s1)?.get(s2)?.let { return it }
+            val c = WorldCopy(0, 0)
+            for (d1 in 1..3) for (d2 in 1..3) for (d3 in 1..3) {
+                val p1n = (p1 + d1 + d2 + d3 - 1) % 10 + 1
+                val s1n = s1 + p1n
+                if (s1n >= 21) {
+                    c.w1++
+                } else {
+                    val cn = find(p2, p1n, s2, s1n)
+                    c.w1 += cn.w2
+                    c.w2 += cn.w1
+                }
+            }
+            cube?.get(p1)?.get(p2)?.get(s1)?.set(s2, c)
+            return c
+        }
+    }
+}
+
 fun main() {
     val data = readInputFile("day21")
+    val players = IntArray(2)
+    for (i in 0..1) {
+        players[i] = data[i].removePrefix("Player ${i + 1} starting position: ").toInt()
+    }
+
     fun part1(): Long {
-        val p = IntArray(2)
-        for (i in 0..1) {
-            p[i] = data[i].removePrefix("Player ${i + 1} starting position: ").toInt()
-        }
+        val p = players.clone()
         var rolls = 0L
         val s = IntArray(2)
         var i = 0
@@ -23,8 +51,11 @@ fun main() {
         }
         return i.toLong()
     }
-    fun part2(): Int {
-        return 0
+
+    fun part2(): Long {
+        WorldCopy.cube = Cube(11) { Array(11) { Array(21) { arrayOfNulls(21) } } }
+        val result = WorldCopy.find(players[0], players[1], 0, 0)
+        return maxOf(result.w1, result.w2)
     }
 
     println("Result part1: ${part1()}")
